9b154fdeb56fc902bd68757d54be1a2349573ab6
wanta zulfikri <109355344+fikri121995@users.noreply.github.com>
2022-11-26 21:22:52 +0700

    Add files via upload

---
+++ python projects/email slicer.py
@@ -0,0 +1,16 @@
+def main():
+    print(" welcome to the email slicer")
+    print("")
+    
+    email_input = input("Input your email address: ")
+    
+    (username, domain) = email_input.split("@")
+    (domain, extension) = domain.split(".")
+    
+    print("username : ", username)
+    print("Domain :", domain)
+    print("Extension :", extension)
+    
+    
+while True:
+    main()
